5761c6a3033e3bab1a7f9b6de217cbb97917dd33
cmccormack <mccormack.christopher@gmail.com>
2017-10-25 20:10:05 -0700

    rename app.js server.js

---
+++ server.js
@@ -0,0 +1,55 @@
+require('dotenv').config()
+const express = require('express')
+const mongo = require('mongodb').MongoClient
+const cons = require('consolidate')
+const assert = require('assert')
+
+const routes = require('./app/routes')
+const api = require('./app/api')
+
+const app = express()
+const port = process.env.PORT || 3000
+
+app.engine('html', cons.nunjucks)
+app.set('view engine', 'html')
+app.set('views', __dirname + '/views')
+
+
+
+
+// Connect to MongoDB database
+mongo.connect(process.env.MONGO_URI, (err, db) => {
+
+  // If error, call error handler middlware 
+  assert.equal(null, err)
+
+  console.log("Successfully connected to MongoDB.");
+
+  // Cross-Origin Header Middleware
+  app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*")
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
+    next();
+  })
+
+  routes(app, db)
+  api(app, db)
+
+  // Error Handler Middleware
+  app.use((err, req, res, next) => {
+    console.error(err.message)
+    console.error(err.stack)
+    res.status(500)
+    res.send(`Error: ${err.message}`)
+  })
+  
+  // All Middleware functions and routes exhausted, respond with 404
+  app.use(function (req, res, next) {
+    res.status(404).send("404 Page Not Found")
+  })
+  
+  const server = app.listen(port, () => {
+    console.log(`Server started on port ${port}.`)
+  })
+
+})
